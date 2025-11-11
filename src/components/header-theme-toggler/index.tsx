ab35448a575a537b3851cf9b3f70f500a36dc2e0
William Alexander <william.al.avila@gmail.com>
2025-11-10 23:04:10 -0300

    feat: Enhance theme toggler:  * Improved styles and animation

--- src/components/header-theme-toggler/index.tsx
+++ src/components/header-theme-toggler/index.tsx
@@ -3,9 +3,10 @@ import '../../assets/styles/header.css'
 import { LinkReturnHome } from "../link-return-home";
 
 import { useLocation } from "react-router";
+import { motion } from "motion/react";
 
 export const HeaderThemeToggler = () => {
-    const { toggleTheme } = useTheme();
+    const { theme, toggleTheme } = useTheme();
     const location = useLocation()
 
     const excludeLinkLocations = ['/', '/teu-espaco-virtual']
@@ -14,9 +15,25 @@ export const HeaderThemeToggler = () => {
     return (
         <header className="header">
             <LinkReturnHome show={showLink} />
-            <button className="theme-toggler" onClick={toggleTheme}>
-                Obrigado
-            </button>
+
+            <div className="theme-toggler-container">
+                <span>Trocar tema</span>
+                <button className="theme-toggler" onClick={toggleTheme}
+                    style={
+                        theme === 'light' ?
+                            { justifyContent: 'flex-start' } :
+                            { justifyContent: 'flex-end' }
+                    }>
+                    <motion.div className="handle"
+                        layout
+                        transition={{
+                            type: "spring",
+                            visualDuration: 0.2,
+                            bounce: 0.2,
+                        }}
+                    />
+                </button>
+            </div>
         </header>
     )
 }
